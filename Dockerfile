c3ee3045ce12fa18c0d1b06f327ad41835a3cc44
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-13 22:50:37 -0300

    feat: cobra cli and init

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM golang
+
 WORKDIR /app/src
+
 COPY . /app/src/
+
+ENV PATH="/go/bin:${PATH}"
+
+RUN go install github.com/spf13/cobra-cli@latest
+
 CMD [ "tail", "-f" ]
--- main.go
+++ main.go
@@ -1,33 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-
-	"github.com/spf13/cobra"
-)
-
-var rootCmd = &cobra.Command{
-	Use:   "Uni",
-	Short: "Uni - Uma CLI da faculdade",
-
-	Run: func(cmd *cobra.Command, args []string) {
-		n1, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("oi")
-		}
-		n2, err := strconv.Atoi(args[1])
-		if err != nil {
-			fmt.Println("oi")
-		}
-		fmt.Println(n1 + n2)
-	},
-}
+import "github.com/KaueSabinoSRV17/UniCrawlerCLI/cmd"
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
