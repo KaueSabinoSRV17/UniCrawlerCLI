c253ac16935298d4eff52f0d49f169616b4f1106
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-14 00:29:01 +0000

    feat: basic cobra cli

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "Uni",
+	Short: "Uni - Uma CLI da faculdade",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		n1, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("oi")
+		}
+		n2, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("oi")
+		}
+		fmt.Println(n1 + n2)
+	},
+}
 
 func main() {
-	fmt.Println("Oi")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
